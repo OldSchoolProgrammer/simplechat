4d9c88f07a8a7e7ddeab5b49f838a5e1589fd53c
George Petropoulos <gpetropoulos@hotmail.com>
2021-04-29 22:11:12 +0300

    Testing channels config

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": ['basicchat.herokuapp.com']
+            "hosts": [('localhost', 6379)]
         },
     },
 }
