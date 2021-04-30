b13137636112c0df4bc5845d8fb694ca7d69a925
George Petropoulos <gpetropoulos@hotmail.com>
2021-04-30 04:17:48 +0300

    optimized Django setup in asgi file

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 import django
+import channels
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
